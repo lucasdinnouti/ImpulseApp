a513af30d8be13e7d45839aceadb2842af08e2a5
Lucas Hideki Carvalho Dinnouti <ldinnouti@gmail.com>
2017-07-11 12:22:33 -0300

    Listagem de publicações com tipo definido por parâmetro da função

--- www/Tudo/_script/script.js
+++ www/Tudo/_script/script.js
@@ -1,61 +1,61 @@
 // http://bookshelf.impulse-157223.appspot.com
 // class XMLHttpRequest()
+function atualizar(tipoDePostagem) {
+    var http = new XMLHttpRequest();
+    //var url = "http://bookshelf.impulse-157223.appspot.com/request";
+    var url = "http://localhost:8080/request?postType="+tipoDePostagem;
+    var response_data;
 
-function atualizar(){
-  var http = new XMLHttpRequest();
-  var url = "http://bookshelf.impulse-157223.appspot.com/request";
-  var response_data;
+    //Abre a url no obrjeto http
+    http.open("GET", url, true);
+    //http.responseType = "json";
+    //Recebe a resposta;
+    //http.setRequestHeader("Content-type", url);
+    http.onreadystatechange = function() {
+        if ((http.readyState == 4) && (http.status == 200)) {
+            response_data = http.responseText;
 
-  //Abre a url no obrjeto http
-  http.open("GET", url, true);
-  //http.responseType = "json";
-  //Recebe a resposta;
-  //http.setRequestHeader("Content-type", url);
-  http.onreadystatechange = function() {
-    if((http.readyState == 4) && (http.status == 200)) {
-      response_data = http.responseText;
+            var json = JSON.parse(response_data);
+            console.log(json.length);
 
-      var json = JSON.parse(response_data);
-      console.log(json.length);
+            for (var i = 0; i < json.length; i++) {
+                var title_data = json[i].title;
+                var author_data = json[i].author;
+                var date_data = json[i].date;
+                var content_data = json[i].content;
 
-      for (var i = 0; i < json.length; i++) {
-        var title_data = json[i].title;
-        var author_data = json[i].author;
-        var date_data = json[i].date;
-        var content_data = json[i].content;
+                //Cria  a publicação na página
+                var div_principal = document.getElementById("public_page");
+                var div_publicacao = document.createElement("div");
+                var title_element = document.createElement("h1");
+                var title_text = document.createTextNode(title_data);
+                var author_element = document.createElement("h2");
+                var author_text = document.createTextNode(author_data);
+                var date_element = document.createElement("h3");
+                var date_text = document.createTextNode(date_data);
+                var content_element = document.createElement("p");
+                var content_text = document.createTextNode(content_data);
 
-        //Cria  a publicação na página
-        var div_principal = document.getElementById("public_page");
-        var div_publicacao = document.createElement("div");
-        var title_element = document.createElement("h1");
-        var title_text = document.createTextNode(title_data);
-        var author_element = document.createElement("h2");
-        var author_text = document.createTextNode(author_data);
-        var date_element = document.createElement("h3");
-        var date_text = document.createTextNode(date_data);
-        var content_element = document.createElement("p");
-        var content_text = document.createTextNode(content_data);
+                div_publicacao.id = "public";
+                title_element.id = "public_title";
+                author_element.id = "public_author";
+                date_element.id = "public_date";
+                content_element.id = "public_content";
 
-        div_publicacao.id = "public";
-        title_element.id = "public_title";
-        author_element.id = "public_author";
-        date_element.id = "public_date";
-        content_element.id = "public_content";
-
-        title_element.appendChild(title_text);
-        author_element.appendChild(author_text);
-        date_element.appendChild(date_text);
-        content_element.appendChild(content_text);
-        div_publicacao.appendChild(title_element);
-        div_publicacao.appendChild(author_element);
-        div_publicacao.appendChild(date_element);
-        div_publicacao.appendChild(content_element);
-        div_principal.appendChild(div_publicacao);
-      }
-    }
+                title_element.appendChild(title_text);
+                author_element.appendChild(author_text);
+                date_element.appendChild(date_text);
+                content_element.appendChild(content_text);
+                div_publicacao.appendChild(title_element);
+                div_publicacao.appendChild(author_element);
+                div_publicacao.appendChild(date_element);
+                div_publicacao.appendChild(content_element);
+                div_principal.appendChild(div_publicacao);
+            }
+        }
 
     }
 
     //Envia o Pedido
     http.send();
-  }
+}
