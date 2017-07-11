aa36a68c815fc964c281b4be9fe1da92b81a20e0
Lucas Hideki Carvalho Dinnouti <ldinnouti@gmail.com>
2017-07-10 22:03:09 -0300

    listagem das publicações, por tipo definido

--- www/Tudo/_script/script.js
+++ www/Tudo/_script/script.js
@@ -3,30 +3,29 @@
 
 function atualizar(){
   var http = new XMLHttpRequest();
-  var url = "http://bookshelf.impulse-157223.appspot.com/lists/";
+  var url = "http://bookshelf.impulse-157223.appspot.com/request";
   var response_data;
 
   //Abre a url no obrjeto http
-  http.open("POST", url, true);
+  http.open("GET", url, true);
   //http.responseType = "json";
   //Recebe a resposta;
   //http.setRequestHeader("Content-type", url);
   http.onreadystatechange = function() {
     if((http.readyState == 4) && (http.status == 200)) {
       response_data = http.responseText;
-    }
 
       var json = JSON.parse(response_data);
-      console.log(json.posts.length);
+      console.log(json.length);
 
-      for (var i = 0; i < json.posts.length; i++) {
-        var title_data = json.posts[i].title;
-        var author_data = json.posts[i].author;
-        var date_data = json.posts[i].date;
-        var content_data = json.posts[i].content;
+      for (var i = 0; i < json.length; i++) {
+        var title_data = json[i].title;
+        var author_data = json[i].author;
+        var date_data = json[i].date;
+        var content_data = json[i].content;
 
         //Cria  a publicação na página
-        var div_principal = document.getElementById("post_page");
+        var div_principal = document.getElementById("public_page");
         var div_publicacao = document.createElement("div");
         var title_element = document.createElement("h1");
         var title_text = document.createTextNode(title_data);
@@ -53,6 +52,7 @@ function atualizar(){
         div_publicacao.appendChild(content_element);
         div_principal.appendChild(div_publicacao);
       }
+    }
 
     }
 
