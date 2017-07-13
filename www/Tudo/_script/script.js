dd6fa92643fa5b16d35d5c7417c2cc43f5bf0d20
Lucas Hideki Carvalho Dinnouti <ldinnouti@gmail.com>
2017-07-13 15:07:06 -0300

    Ajuste de estilo da exibição das postagens

--- www/Tudo/_script/script.js
+++ www/Tudo/_script/script.js
@@ -28,36 +28,48 @@ function Atualizar(tipoDePostagem) {
 			for (var i = 0; i < json.length; i++) {
 				var title_data = json[i].title;
 				var author_data = json[i].author;
-				var date_data = json[i].date;
-				var content_data = json[i].content;
+				var date_data = json[i].publishedDate;
+				var content_data = json[i].description;
 
-				//Cria  a publicação na página
-				var div_principal = document.getElementById("public_page");
-				var div_publicacao = document.createElement("div");
-				var title_element = document.createElement("h1");
-				var title_text = document.createTextNode(title_data);
-				var author_element = document.createElement("h2");
-				var author_text = document.createTextNode(author_data);
-				var date_element = document.createElement("h3");
-				var date_text = document.createTextNode(date_data);
-				var content_element = document.createElement("p");
-				var content_text = document.createTextNode(content_data);
-
-				div_publicacao.id = "public";
-				title_element.id = "public_title";
-				author_element.id = "public_author";
-				date_element.id = "public_date";
-				content_element.id = "public_content";
-
-				title_element.appendChild(title_text);
-				author_element.appendChild(author_text);
-				date_element.appendChild(date_text);
-				content_element.appendChild(content_text);
-				div_publicacao.appendChild(title_element);
-				div_publicacao.appendChild(author_element);
-				div_publicacao.appendChild(date_element);
-				div_publicacao.appendChild(content_element);
-				div_principal.appendChild(div_publicacao);
+				
+				document.getElementById("public_page").innerHTML += 
+				" <div class='panel panel-default'> " +
+				"   <div class='panel-heading'> " +
+				" 	<h3 class='panel-title'>" + title_data + " <small>- " + author_data + "</small></h3> " +
+				"   </div> " +
+				"   <div class='panel-body'> " +
+				" 	" + content_data +
+				" 	<br><br><small> Publicado em " + date_data + "</small>"
+				"   </div> " +
+				" </div> ";
+				
+//				//Cria  a publicação na página
+//				var div_principal = document.getElementById("public_page");
+//				var div_publicacao = document.createElement("div");
+//				var title_element = document.createElement("h1");
+//				var title_text = document.createTextNode(title_data);
+//				var author_element = document.createElement("h2");
+//				var author_text = document.createTextNode(author_data);
+//				var date_element = document.createElement("h3");
+//				var date_text = document.createTextNode(date_data);
+//				var content_element = document.createElement("p");
+//				var content_text = document.createTextNode(content_data);
+				
+//				div_publicacao.id = "public";
+//				title_element.id = "public_title";
+//				author_element.id = "public_author";
+//				date_element.id = "public_date";
+//				content_element.id = "public_content";
+//
+//				title_element.appendChild(title_text);
+//				author_element.appendChild(author_text);
+//				date_element.appendChild(date_text);
+//				content_element.appendChild(content_text);
+//				div_publicacao.appendChild(title_element);
+//				div_publicacao.appendChild(author_element);
+//				div_publicacao.appendChild(date_element);
+//				div_publicacao.appendChild(content_element);
+//				div_principal.appendChild(div_publicacao);
 			}
 		}
 
@@ -142,7 +154,7 @@ function EnviarPost()
 			if (response_data == "NOTOK")
 				alert("Houve um erro na postagem, tente novamente mais tarde.")
 			console.log(response_data);
-			// feedback
+			alert("Postagem executada com sucesso.");
 		}
 		else
 		if (http.readyState == 4) {
