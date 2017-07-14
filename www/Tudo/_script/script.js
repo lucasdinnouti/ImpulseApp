730444c731447c2cd9dcd70abd854c49470a7a38
Lucas Hideki Carvalho Dinnouti <ldinnouti@gmail.com>
2017-07-13 23:05:29 -0300

    teste de upload de imagem

--- www/Tudo/_script/script.js
+++ www/Tudo/_script/script.js
@@ -7,8 +7,8 @@ function Atualizar(tipoDePostagem) {
 	div_debug.innerHTML = "<br>" +tipoDePostagem ;
 	document.getElementById("public_page").appendChild(div_debug);
 	var http = new XMLHttpRequest();
-	var url = "http://bookshelf.impulse-157223.appspot.com/request?postType="+tipoDePostagem;
-	//var url = "http://localhost:8080/request?postType="+tipoDePostagem;
+//	var url = "http://bookshelf.impulse-157223.appspot.com/request?postType="+tipoDePostagem;
+	var url = "http://localhost:8080/request?postType="+tipoDePostagem;
 	var response_data;
 
 	//Abre a url no obrjeto http
@@ -19,7 +19,7 @@ function Atualizar(tipoDePostagem) {
 	http.onreadystatechange = function() {
 		div_debug.innerHTML += "<br> http readystate:" + http.readyState;
 		div_debug.innerHTML += "<br> http status:" + http.status;
-		if ((http.readyState == 4) && ((http.status == 200)||(http.status == 0))) {
+		if ((http.readyState == 4) && ((http.status === 200)||(http.status === 0))) {
 			response_data = http.responseText;
 
 			var json = JSON.parse(response_data);
@@ -72,11 +72,27 @@ function Atualizar(tipoDePostagem) {
 //				div_principal.appendChild(div_publicacao);
 			}
 		}
-
 	}
-	//Envia o Pedido
+	//Envia o Pedido;
 	http.send();
 }
+
+
+
+function toDataURL(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
+}
+
 
 function EnviarPost()
 {
@@ -84,89 +100,96 @@ function EnviarPost()
 	var author = document.getElementById("author").value;
 	var publishedDate = document.getElementById("publishedDate").value;
 	var description = document.getElementById("description").value;
+	var imageUrl = document.getElementById("imageUrl").value;
+	var dataUrl;
 
-	if ((title == "")||(author == "")||(publishedDate == "")||(description == "")) {
-		alert("Preencha todos os campos");
-	}
+	toDataURL(imageUrl, function(dataUrl) {
+		console.log('RESULT:', dataUrl)
+		var data = JSON.stringify({
+		"title": title,
+		"author": author,
+		"publishedDate": publishedDate,
+		"description": description,
+		"imageUrl": dataUrl});
 
-	var data = JSON.stringify({
-	"title": title,
-	"author": author,
-	"publishedDate": publishedDate,
-	"description": description});
+		alert(dataUrl);
 
-//	encodeURI(
-//	"title="+ title +
-//	"&author="+ author +
-//	"&publishedDate="+ publishedDate +
-//	"&description="+ description +
-//	"&file="+
-//	"&id="+
-//	"&imageUrl=");
-
-//	new FormData();
-//	data.append("title", title);
-//	data.append("author", author);
-//	data.append("publishedDate", publishedDate);
-//	data.append("description", description);
-//
-
-
-	var kinds = document.getElementById("kind");
-	var kindSelected = kinds.options[kinds.selectedIndex].value;
-
-	var http = new XMLHttpRequest();
- 	var url = "http://bookshelf.impulse-157223.appspot.com/";
-	//var url = "http://localhost:8080/";
-
-	switch (kindSelected){
-		case "Default":
-			url += "createBook";
-			break;
-		case "Schedule":
-			url += "createSchedule";
-			break;
-		case "Survey":
-			url += "posts" // Tem que ser criado um servlet
-			break;
-		case "Video":
-			url += "posts" // Tem que ser criado um servlet
-			break;
-		default:
-			url += "posts"
-			break;
-	}
-
-	console.log(kindSelected);
-	console.log(url);
-	console.log(data);
-
-	var response_data;
-	//Abre a url no obrjeto http
-	http.open("POST", url, true);
-	//http.responseType = "json";
-	//Recebe a resposta;
-	http.setRequestHeader("Content-type", "application/json");
-	http.onreadystatechange = function() {
-		if ((http.readyState == 4) && (http.status == 200)) {
-			response_data = http.responseText;
-			if (response_data == "NOTOK")
-				alert("Houve um erro na postagem, tente novamente mais tarde.")
-			console.log(response_data);
-			alert("Postagem executada com sucesso.");
+		if ((title == "")||(author == "")||(publishedDate == "")||(description == "")) {
+			alert("Preencha todos os campos");
 		}
-		else
-		if (http.readyState == 4) {
+	//	encodeURI(
+	//	"title="+ title +
+	//	"&author="+ author +
+	//	"&publishedDate="+ publishedDate +
+	//	"&description="+ description +
+	//	"&file="+
+	//	"&id="+
+	//	"&imageUrl=");
 
-			switch (http.status) {
-				case 404:
-					break;
-				case 405:
-					break;
-				default:
-					break;
+	//	new FormData();
+	//	data.append("title", title);
+	//	data.append("author", author);
+	//	data.append("publishedDate", publishedDate);
+	//	data.append("description", description);
+	//
+
+
+		var kinds = document.getElementById("kind");
+		var kindSelected = kinds.options[kinds.selectedIndex].value;
+
+		var http = new XMLHttpRequest();
+//		var url = "http://bookshelf.impulse-157223.appspot.com/";
+		var url = "http://localhost:8080/";
+
+		switch (kindSelected){
+			case "Default":
+				url += "createBook";
+				break;
+			case "Schedule":
+				url += "createSchedule";
+				break;
+			case "Survey":
+				url += "posts" // Tem que ser criado um servlet
+				break;
+			case "Video":
+				url += "posts" // Tem que ser criado um servlet
+				break;
+			default:
+				url += "posts"
+				break;
+		}
+
+		console.log(kindSelected);
+		console.log(url);
+		console.log(data);
+
+		var response_data;
+		//Abre a url no obrjeto http
+		http.open("POST", url, true);
+		//http.responseType = "json";
+		//Recebe a resposta;
+		http.setRequestHeader("Content-type", "application/json");
+		http.onreadystatechange = function() {
+			if ((http.readyState == 4) && (http.status == 200)) {
+				response_data = http.responseText;
+				if (response_data == "NOTOK")
+					alert("Houve um erro na postagem, tente novamente mais tarde.")
+				console.log(response_data);
+				alert("Postagem executada com sucesso.");
+			}
+			else
+			if (http.readyState == 4) {
+
+				switch (http.status) {
+					case 404:
+						break;
+					case 405:
+						break;
+					default:
+						break;
+				}
 			}
 		}
-	}
-	http.send(data);
+		http.send(data);
+	})
 }
